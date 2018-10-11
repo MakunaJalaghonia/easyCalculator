23f6bc0df2a3869dd445f98183d2927b6aa45f9b
makuna <jalaghonia.makuna@gmail.com>
2018-10-12 03:17:31 +0400

    ავტვირთე easycalculatorის კოდი
    ავტვირთე ჩემი პროექტის კოდი, მოვხსენი კომენტარი switch case-ს ნაწილს და გადასამოწმებლად პროგრამის გასაშვებად დავაკომენატე if else ნაწილი.

---
+++ EasyCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int num1, num2;
+			string symbol;
+
+			Console.WriteLine("daweret pirveli ritskhvi: ");
+			num1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("daweret matematikuri operacia: ");
+			symbol = Console.ReadLine();
+			Console.WriteLine("daweret meore ritskhvi: ");
+			num2 = Convert.ToInt32(Console.ReadLine());
+
+			switch (symbol)
+
+			{
+				case "+": Console.WriteLine("pasuxi aris : " + (num1 + num2)); break;
+				case "-": Console.WriteLine("pasuxi aris : " + (num1 - num2)); break;
+				case "/": Console.WriteLine("pasuxi aris : " + num1 / num2); break;
+				case "*": Console.WriteLine("pasuxi aris : " + num1 * num2); break;
+				default: Console.WriteLine("daweret swori matematikuri operacia"); break;
+
+			}
+
+
+
+			//if (symbol == "+")
+
+			//{
+				//Console.WriteLine("pasuxi aris: " + (num1 + num2));
+			//}
+			//else if (symbol == "-")
+			//{
+				//Console.WriteLine("pasuxi aris: " + (num1 - num2));
+			//}
+			//else if (symbol == "/")
+			//{
+				//Console.WriteLine("pasuxi aris: " + num1 / num2);
+			//}
+			//else if (symbol == "*")
+			//{
+				//Console.WriteLine("pasuxi aris: " + num1 * num2);
+			//}
+
+			//else
+
+			//{
+				//Console.WriteLine("daweret swori matematikuri operacia");
+
+			//}
+
+
+
+
+
+
+
+               
+
+
+
+
+		
+
